569b46ae292a2a72ddf477efa6f42a534f60b4a2
John Stephen Burky <jburky@eceprog6.ecn.purdue.edu>
2025-04-02 13:31:32 -0400

    linked_list functions work

--- linked_list.c
+++ linked_list.c
@@ -2,15 +2,52 @@
 
 #include <stdlib.h>
 
-struct list_node *new_node(size_t value) { return NULL; }
+struct list_node *new_node(size_t value) 
+{
+  struct list_node* new = malloc(sizeof(struct list_node));
+  new -> next = NULL;
+  new -> value = value;
+  return new;
+}
 
-void insert_at_head(struct linked_list *list, size_t value) {}
+void insert_at_head(struct linked_list *list, size_t value) 
+{
+  struct list_node *in = new_node(value);
+  in -> next = list -> head;
+  list -> head = in;
+}
 
-void insert_at_tail(struct linked_list *list, size_t value) {}
+void insert_at_tail(struct linked_list *list, size_t value) 
+{
+  struct list_node *end = list -> head;
+  while((end -> next) != NULL)
+  {
+    end = end -> next;
+  }
+  end -> next = new_node(value);
+}
 
-size_t remove_from_head(struct linked_list *list) { return 0; }
+size_t remove_from_head(struct linked_list *list) 
+{
+  struct list_node *q = list -> head; 
+  list -> head = list -> head -> next;
+  free(q);
+  return list -> head -> value; 
+}
 
-size_t remove_from_tail(struct linked_list *list) { return 0; }
+size_t remove_from_tail(struct linked_list *list) 
+{
+  struct list_node *p = list -> head;  
+  struct list_node *q = p -> next;
+  while((q -> next) != NULL)
+  {
+    q = q -> next;
+    p = p -> next;
+  }
+  p -> next = NULL;
+  free(q); 
+  return p -> value; 
+}
 
 void free_list(struct linked_list list) {}
 
